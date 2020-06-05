20d648d957f04f7b4c71a9605b1791bc34b9d971
songshuiyang <1459074711@qq.com>
2020-06-05 18:32:37 +0800

    nacos

---
+++ cloud-alibaba-config/src/main/java/cloud/config/NacosConfigApplication.java
@@ -0,0 +1,17 @@
+package cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author songsy
+ * @date 2019/11/1 16:51
+ */
+@SpringBootApplication
+public class NacosConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class, args);
+    }
+
+}
---
+++ cloud-alibaba-config/src/main/java/cloud/config/controller/ConfigController.java
@@ -0,0 +1,28 @@
+package cloud.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author songsy
+ * @date 2020/6/5 16:52
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    /**
+     * http://localhost:8080/config/get
+     */
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+
+}
